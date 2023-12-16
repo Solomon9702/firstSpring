df0bbfcd1c23fe56fd63bacb8f8441cbdb2a7efe
chanho <thdcksgh15@gmail.com>
2023-12-17 00:42:17 +0900

    Add transaction

--- firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -3,11 +3,15 @@ package com.example.firstproject.service;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j // 로그 정보 확인
 @Service  // 서비스 선언! (서비스 객체를 스프링부트에 생성)
@@ -49,5 +53,40 @@ public class ArticleService {
         target.patch(article);
         Article updated = articleRepository.save(target);
         return updated;
+    }
+
+    public Article delete(Long id) {
+        // 대상 찾기
+        Article target= articleRepository.findById(id).orElse(null);
+
+        // 잘못된 요청처리
+        if(target == null){
+            return null;
+        }
+
+        // 대상 삭제 후 반환
+        articleRepository.delete(target);
+        return target;
+
+    }
+
+    @Transactional // 해당 메소드를 트랜잭션으로 묶는다!
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // dto 묶음을 entity 묶음으로 변환
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+        // entity 묶음을 DB로 저장
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+        // 강제 예외 발생
+        articleRepository.findById(-1L).orElseThrow(
+                () -> new IllegalArgumentException("결제 실패!")
+        );
+
+        // 결과값 반환
+        return articleList;
     }
 }
